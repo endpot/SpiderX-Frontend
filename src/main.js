852263f5be0bcda99fbef7055ea0010eb0a58f36
ajycc20 <ajycc20@qq.com>
2019-06-22 16:01:47 +0800

    更新到最新模板，forum基本框架

--- src/main.js
+++ src/main.js
@@ -9,12 +9,29 @@ import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 import '@/styles/index.scss' // global css
 
 import App from './App'
-import router from './router'
 import store from './store'
+import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
 
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
+
+import Axios from 'axios'
+Vue.prototype.$axios = Axios // 暂时使用
+
+// set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,119 +1,177 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
-// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
-
 Vue.use(Router)
 
 /* Layout */
-import Layout from '../views/layout/Layout'
+import Layout from '@/layout'
 
 /**
-* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
-* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
-*                                if not set alwaysShow, only more than one route under the children
-*                                it will becomes nested mode, otherwise not show the root menu
-* redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
-* name:'router-name'             the name is used by <keep-alive> (must set!!!)
-* meta : {
-    title: 'title'               the name show in submenu and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon show in the sidebar,
+ * Note: sub-menu only appear when route children.length >= 1
+ * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * hidden: true                   if set true, item will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar
+    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
   }
-**/
-export const constantRouterMap = [
-  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
-  { path: '/404', component: () => import('@/views/404'), hidden: true },
+ */
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
 
   {
     path: '/',
     component: Layout,
     redirect: '/home',
-    name: 'Home',
-    children: [{
-      path: 'home',
-      component: () => import('@/views/home/index'),
-      meta: { title: 'Home', icon: 'home' }
-    }]
+    children: [
+      {
+        path: 'home',
+        name: 'Home',
+        component: () => import('@/views/home/index'),
+        meta: {
+          title: 'Home',
+          icon: 'home'
+        }
+      }
+    ]
   }
 ]
 
-export const asyncRouterMap = [
+/**
+ * asyncRoutes
+ * the routes that need to be dynamically loaded based on user roles
+ */
+export const asyncRoutes = [
   {
     path: '/forum',
     component: Layout,
-    children: [{
-      path: 'index',
-      name: 'Forum',
-      component: () => import('@/views/home/index'),
-      meta: { title: 'Forum', icon: 'forum' }
-    }]
+    children: [
+      {
+        path: 'index',
+        name: 'Forum',
+        component: () => import('@/views/forum/index'),
+        meta: {
+          title: 'Forum',
+          icon: 'forum'
+        }
+      }
+    ]
   },
-
   {
     path: '/torrent',
     component: Layout,
-    children: [{
-      path: 'index',
-      name: 'Torrent',
-      component: () => import('@/views/home/index'),
-      meta: { title: 'Torrent', icon: 'torrent' }
-    }]
+    children: [
+      {
+        path: 'index',
+        name: 'Torrent',
+        component: () => import('@/views/home/index'),
+        meta: {
+          title: 'Torrent',
+          icon: 'torrent'
+        }
+      }
+    ]
   },
-
   {
     path: '/subtitle',
     component: Layout,
-    children: [{
-      path: 'index',
-      name: 'Subtitle',
-      component: () => import('@/views/home/index'),
-      meta: { title: 'Subtitle', icon: 'subtitle' }
-    }]
+    children: [
+      {
+        path: 'index',
+        name: 'Subtitle',
+        component: () => import('@/views/home/index'),
+        meta: {
+          title: 'Subtitle',
+          icon: 'subtitle'
+        }
+      }
+    ]
   },
 
   {
     path: '/market',
     component: Layout,
-    children: [{
-      path: 'index',
-      name: 'Market',
-      component: () => import('@/views/home/index'),
-      meta: { title: 'Market', icon: 'market' }
-    }]
+    children: [
+      {
+        path: 'index',
+        name: 'Market',
+        component: () => import('@/views/home/index'),
+        meta: {
+          title: 'Market',
+          icon: 'market'
+        }
+      }
+    ]
   },
-
   {
     path: '/game',
     component: Layout,
-    children: [{
-      path: 'index',
-      name: 'Game',
-      component: () => import('@/views/home/index'),
-      meta: { title: 'Game', icon: 'game' }
-    }]
+    children: [
+      {
+        path: 'index',
+        name: 'Game',
+        component: () => import('@/views/home/index'),
+        meta: {
+          title: 'Game',
+          icon: 'game'
+        }
+      }
+    ]
   },
-
   {
     path: '/faq',
     component: Layout,
-    children: [{
-      path: 'index',
-      name: 'FAQ',
-      component: () => import('@/views/home/index'),
-      meta: { title: 'FAQ', icon: 'faq' }
-    }]
+    children: [
+      {
+        path: 'index',
+        name: 'FAQ',
+        component: () => import('@/views/home/index'),
+        meta: {
+          title: 'FAQ',
+          icon: 'faq'
+        }
+      }
+    ]
   },
 
   {
     path: '/about',
     component: Layout,
-    children: [{
-      path: 'index',
-      name: 'About',
-      component: () => import('@/views/home/index'),
-      meta: { title: 'About', icon: 'about' }
-    }]
+    children: [
+      {
+        path: 'index',
+        name: 'About',
+        component: () => import('@/views/home/index'),
+        meta: {
+          title: 'About',
+          icon: 'about'
+        }
+      }
+    ]
   },
 
   {
@@ -121,40 +179,71 @@ export const asyncRouterMap = [
     component: Layout,
     redirect: '/dashboard/meter',
     name: 'Dashboard',
-    meta: { title: 'Dashboard', icon: 'dashboard', roles: ['admin'] },
+    meta: {
+      title: 'Dashboard',
+      icon: 'dashboard',
+      roles: ['admin']
+    },
     children: [
       {
         path: 'meter',
         name: 'Meter',
         component: () => import('@/views/home/index'),
-        meta: { title: 'Meter', icon: 'meter' }
+        meta: {
+          title: 'Meter',
+          icon: 'meter'
+        }
       },
       {
         path: 'user',
         name: 'User',
         component: () => import('@/views/home/index'),
-        meta: { title: 'User', icon: 'user' }
+        meta: {
+          title: 'User',
+          icon: 'user'
+        }
       },
       {
         path: 'torrent',
-        name: 'Torrent',
+        name: 'Dashboard-Torrent',
         component: () => import('@/views/home/index'),
-        meta: { title: 'Torrent', icon: 'torrent' }
+        meta: {
+          title: 'Torrent',
+          icon: 'torrent'
+        }
       },
       {
         path: 'setting',
         name: 'Setting',
         component: () => import('@/views/home/index'),
-        meta: { title: 'Setting', icon: 'setting' }
+        meta: {
+          title: 'Setting',
+          icon: 'setting'
+        }
       }
     ]
   },
 
-  { path: '*', redirect: '/404', hidden: true }
+  // 404 page must be placed at the end !!!
+  {
+    path: '*',
+    redirect: '/404',
+    hidden: true
+  }
 ]
 
-export default new Router({
-  // mode: 'history', //后端支持可开
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
-  routes: constantRouterMap
+  routes: constantRoutes
 })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
