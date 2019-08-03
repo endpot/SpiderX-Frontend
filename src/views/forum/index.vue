4512977ac0bd086f04e26d6a4e58b55b5076b4d7
ajycc20 <ajycc20@qq.com>
2019-08-03 13:28:34 +0800

    将tab封装为单个组件

--- src/views/forum/index.vue
+++ src/views/forum/index.vue
@@ -5,72 +5,9 @@
         <el-col :span="16" :offset="1">
           <el-tabs v-model="activeName" class="tab-container" type="border-card" @tab-click="handleClick">
             <el-tab-pane label="全部" name="all">
+              <!-- tab page 1 -->
+              <tab-page :tab-name="all" />
 
-              <el-table v-loading="listLoading" :data="forumData" :show-header="showHeader" fit highlight-current-row style="width: 100%">
-
-                <el-table-column align="center" label="Icon" width="60">
-                  <template slot-scope="scope">
-                    <router-link :to="'#'">
-                      <el-popover placement="right" trigger="hover">
-                        <el-card class="user-info-hover" shadow="never">
-                          <div slot="header" class="clearfix">
-                            <el-image :src="scope.row.author.avatar_url" fit="fill" style="width: 40px; height: 40px;" />
-                            <div class="user-name"> {{ scope.row.author.author_name }} </div>
-                            <div v-for="role in roles" :key="role" style="float: right; padding: 3px 0">{{ role }}</div>
-                          </div>
-                          <div class="user-options">
-                            <el-row>
-                              <el-button size="small" round>加为好友</el-button>
-                              <el-button size="small" round>发送信息</el-button>
-                              <el-button size="small" round>打个招呼</el-button>
-                            </el-row>
-                          </div>
-                        </el-card>
-                        <el-image slot="reference" :src="scope.row.author.avatar_url" fit="fill" style="width: 40px; height: 40px; border-radius: 50%;" />
-                      </el-popover>
-                    </router-link>
-                  </template>
-                </el-table-column>
-
-                <el-table-column align="left" label="Title" width="auto">
-                  <template slot-scope="{row}">
-                    <div class="titlelist">
-                      <el-button v-if="row.topicType === 'notice'" type="text" @click="changeTab(row)">[站务公告]</el-button>
-                      <el-button v-if="row.topicType === 'guide'" type="text" @click="changeTab(row)">[新手指引]</el-button>
-                      <el-button v-if="row.topicType === 'discuss'" type="text" @click="changeTab(row)">[综合交流]</el-button>
-                      <el-button v-if="row.topicType === 'hobby'" type="text" @click="changeTab(row)">[兴趣爱好]</el-button>
-                      <el-button v-if="row.topicType === 'working'" type="text" @click="changeTab(row)">[站务工作]</el-button>
-                      <router-link :to="'/forum/details/'+row.id" :title="row.title" class="link-type">
-                        <div>{{ row.title }}</div>
-                      </router-link>
-                    </div>
-                    <div class="titleinfo">
-                      <span>作者:</span>
-                      <router-link :to="'/user/info/'+row.author_id" :title="row.author" class="link-type">
-                        <span> {{ row.author.author_name }} </span>
-                      </router-link>
-                      <span> {{ row.create_at }} </span>
-                      <span> | </span>
-                      <span> 最后发表: </span>
-                      <span>{{ row.author.author_name }}</span>
-                      <span>{{ row.last_reply_at | formatLocalTime(row.last_reply_at) }}</span>
-                    </div>
-                  </template>
-                </el-table-column>
-
-                <el-table-column align="right" label="ReplyOrRead" width="120">
-                  <template slot-scope="scope">
-                    <div class="reply-read">
-                      <span>回帖:</span>
-                      <span> {{ scope.row.reply_count }} </span>
-                      <span> / </span>
-                      <span> {{ scope.row.pageviews }} </span>
-                    </div>
-                  </template>
-                </el-table-column>
-              </el-table>
-
-              <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
             </el-tab-pane>
             <!-- 这里需要keep-alive -->
             <el-tab-pane label="站务公告" name="notice">
@@ -78,67 +15,8 @@
               <el-tag v-permission="['admin']">This is admin</el-tag>
               <el-tag v-permission="['editor']">This is editor</el-tag>
               <el-tag v-permission="['admin','editor']">Both admin or editor can see this</el-tag>
-
-              <el-table v-loading="listLoading" :data="noticeList" :show-header="showHeader" fit highlight-current-row style="width: 100%">
-
-                <el-table-column align="center" label="Icon" width="60">
-                  <template slot-scope="scope">
-                    <router-link :to="'#'">
-                      <el-popover placement="right" trigger="hover">
-                        <el-card class="user-info-hover" shadow="never">
-                          <div slot="header" class="clearfix">
-                            <el-image :src="scope.row.author.avatar_url" fit="fill" style="width: 40px; height: 40px;" />
-                            <div class="user-name"> {{ scope.row.author.author_name }} </div>
-                            <div v-for="role in roles" :key="role" style="float: right; padding: 3px 0">{{ role }}</div>
-                          </div>
-                          <div class="user-options">
-                            <el-row>
-                              <el-button size="small" round>加为好友</el-button>
-                              <el-button size="small" round>发送信息</el-button>
-                              <el-button size="small" round>打个招呼</el-button>
-                            </el-row>
-                          </div>
-                        </el-card>
-                        <el-image slot="reference" :src="scope.row.author.avatar_url" fit="fill" style="width: 40px; height: 40px; border-radius: 50%;" />
-                      </el-popover>
-                    </router-link>
-                  </template>
-                </el-table-column>
-
-                <el-table-column align="left" label="Title" min-width="300px">
-                  <template slot-scope="scope">
-                    <div class="titlelist">
-                      <router-link :to="'/forum/details/'+scope.row.id" :title="scope.row.title" class="link-type">
-                        <div>{{ scope.row.title }}</div>
-                      </router-link>
-                    </div>
-                    <div class="titleinfo">
-                      <span>作者:</span>
-                      <router-link :to="'/user/info/'+scope.row.author_id" :title="scope.row.author" class="link-type">
-                        <span> {{ scope.row.author.author_name }} </span>
-                      </router-link>
-                      <span> {{ scope.row.create_at }} </span>
-                      <span> | </span>
-                      <span> 最后发表: </span>
-                      <span>{{ scope.row.author.author_name }}</span>
-                      <span>{{ scope.row.last_reply_at | formatLocalTime(scope.row.last_reply_at) }}</span>
-                    </div>
-                  </template>
-                </el-table-column>
-
-                <el-table-column align="right" label="ReplyOrRead">
-                  <template slot-scope="scope">
-                    <div class="reply-read">
-                      <span>回帖:</span>
-                      <span> {{ scope.row.reply_count }} </span>
-                      <span> / </span>
-                      <span> {{ scope.row.pageviews }} </span>
-                    </div>
-                  </template>
-                </el-table-column>
-              </el-table>
-
-              <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+              <!-- tab page 2 -->
+              <tab-page :tab-name="notice" />
 
             </el-tab-pane>
             <el-tab-pane label="新手指引" name="guide">分享</el-tab-pane>
@@ -181,37 +59,19 @@
 </template>
 
 <script>
-import { formatTime, timeStamp } from '@/utils' // 这里考虑到mockjs生成的日期是yyyy-MM-dd HH:mm:ss  所以封装了timeStamp将时间转为unix格式
-import { fetchList } from '@/api/forum'
 import { mapGetters } from 'vuex'
 import permission from '@/directive/permission'
-import Pagination from '@/components/Pagination'
+import TabPage from './components/Tabs'
 export default {
-  filters: {
-    formatLocalTime(time) {
-      return formatTime(timeStamp(time))
-    }
-  },
   directives: {
     permission
   },
   components: {
-    Pagination
+    TabPage
   },
   data() {
     return {
-      activeName: 'notice',
-      // tab all page
-      forumData: null,
-      total: 0,
-      listLoading: true,
-      listQuery: {
-        page: 1,
-        limit: 20,
-        tab: 'all'
-      },
-      // tab notice page
-      noticeList: null,
+      activeName: 'all',
       front2Back: false,
       showHeader: false
     }
@@ -222,23 +82,10 @@ export default {
       'roles'
     ])
   },
-  created() {
-    this.getList()
-  },
   methods: {
-    getList() {
-      this.listLoading = true
-      fetchList(this.listQuery).then(res => {
-        // console.log(res.data)
-        this.forumData = res.data.items
-        this.total = res.data.total
-        this.listLoading = false
-      })
-    },
     handleClick(tab, event) {
       console.log(tab, event)
       this.listQuery.tab = tab.name
-      this.getList()
     },
     // 每页数
     handleSizeChange(val) {
@@ -256,7 +103,6 @@ export default {
       const idTab = id.row.topicType
       this.activeName = idTab
     }
-
   }
 }
 </script>
